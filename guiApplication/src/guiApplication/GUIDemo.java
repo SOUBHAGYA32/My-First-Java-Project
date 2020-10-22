c1b241a99dc38f79d4885c9b9193848dc25d866b
SOUBHAGYA32 <soubhagya801832@gmail.com>
2020-10-23 00:40:50 +0530

    First Commit

---
+++ guiApplication/src/guiApplication/GUIDemo.java
@@ -0,0 +1,43 @@
+package guiApplication;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class GUIDemo {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUIDemo window = new GUIDemo();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GUIDemo() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 501, 524);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
